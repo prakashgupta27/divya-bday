34c8b501da9f31e2bfcc1b64d249312e165f9eff
prakashgupta27 <prakashgupta272001@gmail.com>
2025-08-25 12:13:59 +0530

    push

--- src/component/birthdayLandingPage.jsx
+++ src/component/birthdayLandingPage.jsx
@@ -40,7 +40,7 @@ const photos = [
     // { image: Image, caption: "Happy birthday Naresh ki friend ko 😏" },
     { image: DiK, caption: "Happy birthday Ek dosti nibhane wali Divya ko ✌️" },
     // { image: Image, caption: "Happy Birthday mummy ki pyari beti ko ❤️" },
-    { image: Image, caption: "Happy birthday Duniya ki sabse Sundar ladki ko 🤩" },
+    // { image: Image, caption: "Happy birthday Duniya ki sabse Sundar ladki ko 🤩" },
     { image: DiP, caption: "Happy birthday pyar ko pyar se nibanne wali Divya ko 🥺" }
 ];  
 
